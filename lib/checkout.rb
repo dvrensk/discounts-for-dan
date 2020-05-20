2cce2bbc2a7a9538f5f60a1d2db28a29376ff0f9
David Vrensk <david@vrensk.com>
2020-05-20 17:51:09 +0200

    [ADD] Checkout#scan that only accepts known products

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,5 +1,18 @@
+# frozen_string_literal: true
+require "bigdecimal"
+
 class Checkout
+  CATALOG = {
+    "GR1" => BigDecimal("3.11"),
+    "SR1" => BigDecimal("5.00"),
+    "CF1" => BigDecimal("11.23"),
+  }.freeze
+
   def initialize(rules)
     @rules = rules
+  end
+
+  def scan(code)
+    CATALOG.fetch(code)
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,12 +1,27 @@
 describe Checkout do
 
+  # | Product code | Name         | Price  |
+  # | ------------ | ----         | ----:  |
+  # | GR1          | Green tea    | £3.11  |
+  # | SR1          | Strawberries | £5.00  |
+  # | CF1          | Coffee       | £11.23 |
+
   let(:checkout) { Checkout.new(rules) }
   let(:rules) { [] }
 
-  context '.new' do
+  describe '.new' do
     it 'creates an instance, given rules' do
       expect(checkout).to be_a Checkout
     end
   end
-  
+
+  describe '#scan' do
+    it 'accepts known product codes' do
+      expect { checkout.scan("GR1") }.not_to raise_error
+    end
+
+    it 'complains about unknown codes' do
+      expect { checkout.scan("XX1") }.to raise_error(KeyError)
+    end
+  end
 end
